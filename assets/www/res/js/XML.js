8c9ef6c2961301a6a8a0e64e66d002293f5eb358
Vadim Richter <richter@cs.uni-bonn.de>
2012-08-28 16:17:03 +0200

    Import of JS stuff from Wiki

---
+++ assets/www/res/js/XML.js
@@ -0,0 +1,206 @@
+function getGameRepository(){
+	gameRepository=loadXMLDoc("http://geoquest.qeevee.org/repositories/repolist.php")
+	alert ("GameRepository: " + gameRepository);
+	var gameQuantity = gameRepository.getElementsByTagName('game').length;
+
+	alert ("GameQuantity" + gameQuantity);
+}
+
+
+function xmlMission(missionNode){
+
+}
+
+//////////
+// xmlRule wird vom Handler aufgerufen onStart, onEnd, onEnter, onLeave aufgerufen
+// Zunaechst wird die Condition für die Rule geprueft.
+// Danach werden die Einzelnen Actions ausgefuehrt.
+////////
+function xmlRule(ruleNode){
+	var doit = true;
+	var ifNode = ruleNode.getElementsByTagName('if')[0];
+	if (ifNode){
+		doit = xmlCheckCondition(ifNode.childNodes[1]); //Bedingungen ueberpruefen
+	}
+	
+	if (doit){
+	   	var actionQuantity = ruleNode.getElementsByTagName('action').length,
+	   		actionIndex;
+
+	   	
+	   	//	Hier koennte man die Action node an den ActionHandler uebergeben, dann wuerde das switch hier verschwinden
+	   	// 	und man mueßte die Actions (auch zukuenftige) nur an einer stelle pflegen
+		
+	   	for (actionIndex=0; actionIndex < actionQuantity; actionIndex++){
+	   		var actionAttributes = ruleNode.getElementsByTagName('action')[actionIndex].attributes;
+	   			actionType = actionAttributes.getNamedItem("type").nodeValue;
+	   			
+			switch (actionType){
+				case "EndGame":
+					globalActionHandler.EndGame();
+					break;
+				case "SetVariable":
+					var Variable = actionAttributes.getNamedItem("var").nodeValue,
+						actionNode = ruleNode.getElementsByTagName('action')[actionIndex];
+
+					if (actionNode.getElementsByTagName('bool').length){
+						boolNode = actionNode.getElementsByTagName('bool')[0].firstChild;
+						Value = boolNode.nodeValue;
+					}
+					if (actionNode.getElementsByTagName('num').length) {
+						numNode = actionNode.getElementsByTagName('num')[0].firstChild;
+						Value = numNode.nodeValue;
+					}		
+					globalActionHandler.SetVariable(Variable,Value);
+					break;
+				case "IncrementVariable":
+					var Variable = actionAttributes.getNamedItem("var").nodeValue;
+					globalActionHandler.IncrementVariable(Variable);
+					break;
+				case "DecrementVariable":
+					var Variable = actionAttributes.getNamedItem("var").nodeValue;
+					globalActionHandler.DecrementVariable(Variable);
+					break;
+				case "PlayAudio":
+					var AudioFile = actionAttributes.getNamedItem("file").nodeValue;
+					globalActionHandler.PlayAudio(AudioFile);
+					break;
+				case "Vibrate":
+					var Duration = 500;
+					if (actionAttributes.getNamedItem("duration")){
+						var Duration = actionAttributes.getNamedItem("duration").nodeValue;
+					}
+					globalActionHandler.Vibrate(Duration);
+					break;
+				case "ShowMessage":
+					var Message = actionAttributes.getNamedItem("message").nodeValue;
+					globalActionHandler.ShowMessage(Message);
+					break;
+				case "SetHotspotVisibility":
+					var ID = actionAttributes.getNamedItem("id").nodeValue,
+						Value = actionAttributes.getNamedItem("visible").nodeValue;
+					globalActionHandler.SetHotspotVisibility(ID,Value);
+					break;
+				case "StartMission":
+					var ID = actionAttributes.getNamedItem("id").nodeValue;
+					globalActionHandler.StartMission(ID);
+					break;
+				case "StartExternalMission":
+					globalActionHandler.StartExternalMission();
+					break;
+				default:
+					alert("Unbekannter ActionType gefunden: " + actionType);
+					break;
+			}
+		} 	
+	}
+}
+
+function xmlCheckCondition(conditionNode){
+	var i,
+		result;
+	
+	switch (conditionNode.nodeName){
+		case "and":
+			result = true;
+			var childLength = conditionNode.childNodes.length;
+		   	for (var i = 0; i < childLength; i++){
+		   			if (conditionNode.childNodes[i].nodeType === 1){
+		   				result = result & xmlCheckCondition(conditionNode.childNodes[i]);
+		   			}
+		   	}
+			return result;
+			break;		
+		case "or":
+			result = false;
+
+			var childLength = conditionNode.childNodes.length;
+		   	for (var i = 0; i < childLength; i++){
+		   			if (conditionNode.childNodes[i].nodeType === 1){
+		   				result = result | xmlCheckCondition(conditionNode.childNodes[i]);
+		   			}
+		   	}
+			return result;
+			break;			
+		
+		case "not":
+			if (xmlCheckCondition(conditionNode.childNodes[1])){
+				return false;
+			}
+			else{
+				return  true;
+			}
+			break;
+		   	
+		case "missionState":
+			if (globalGameHandler.getMissionStatus(conditionNode.attributes.getNamedItem("id").nodeValue) === conditionNode.attributes.getNamedItem("state").nodeValue){
+				return true;
+			}
+			else {
+				return false;
+			}
+			break;
+			
+		case "eq":
+			break;
+			
+		case "gt":
+			break;
+			
+		case "geq":
+			break;
+			
+		case "lt":
+			break;	
+			
+		case "leq":
+			break;
+			
+		default:
+			alert("Fehler beim ConditionCheck - Das sollte Nie passieren!")
+	}
+}
+
+	
+function loadXMLDoc(fileName) {    
+    
+	if (window.XMLHttpRequest) {
+	    xhttp=new XMLHttpRequest();
+	}
+	else {
+	    // Internet Explorer hier aber wohl vernachlaessigbar
+	    xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	if (DEBUG){
+		alert(fileName);
+	}	
+	xhttp.open("GET",fileName,false);
+	
+    xhttp.onreadystatechange = function(evt) {
+
+        if(xhttp.readyState == 4){
+            if (xhttp.status == 200)
+            {
+
+            }
+            else
+            {
+        	    alert("Fehler beim lesen der XML Datei! Status:" +	xhttp.status);
+            }
+        }
+    }
+
+	xhttp.send();
+
+	if (DEBUG){
+		alert("XHTTP Status: " + xhttp.status);
+	}			
+	if (xhttp.status != 200) {
+	    alert("Fehler beim lesen der XML Datei!");
+	}
+	
+	//Notwendig da die RepoListe noch ohne MimeType kommt
+	xhttp.overrideMimeType('text/xml');
+
+	return xhttp.responseXML;
+}
---
+++ assets/www/res/js/missions/MapOverview.js
@@ -0,0 +1,191 @@
+function MapOverview(mapNode) {		
+
+	var mapAttributes = mapNode.attributes,
+		ID = mapAttributes.getNamedItem("id").nodeValue,
+		googleMarkers = {},	
+		latlng = new google.maps.LatLng(50.751843, 7.096065),
+		gpsOptions = {enableHighAccuracy: true},
+		watchID,
+		zoomLevel = 18, 	//Default ZoomLevel
+		myAccuracy = 60; 	// Fixed Accuracy
+
+	if (!GEOQUEST_RESUME){
+			localStorage[localStorage["game"]+ID] = "new";
+	}
+		
+		
+	// Erste Abfrage der Position um die Karte zu zentrieren und erste Daten zu laden.
+	navigator.geolocation.getCurrentPosition(initialCenterMap, gpsError, gpsOptions);
+		
+	//ZoomLevel aus den Attributen auslesen, falls dieser vorhanden ist.
+	if (mapAttributes.getNamedItem("zoomlevel")){
+		zoomLevel =+ mapAttributes.getNamedItem("zoomlevel").nodeValue;
+	}	
+		
+	//Optionen fuer die GoogleMap
+	var mapOverviewOptions = {
+			zoom : zoomLevel,
+			center : latlng,
+			mapTypeControl : false, //MapType darf nicht geaendert werden
+			streetViewControl : false, //Kein streetview
+			mapTypeId : google.maps.MapTypeId.HYBRID // default
+		};		
+	
+	//MapKind aus den Atributen auslesen, falls vorhanden ist.
+	if (mapAttributes.getNamedItem("mapkind")){
+		var mapKind = mapAttributes.getNamedItem("mapkind").nodeValue;
+		
+		//Anpassen der GoogleMaps Optionen mit jeweiligem MapType 
+		switch (mapKind){
+		case "terrain":
+			mapOverviewOptions.mapTypeId= google.maps.MapTypeId.TERRAIN;
+			break;
+		case "roadmap":
+			mapOverviewOptions.mapTypeId= google.maps.MapTypeId.ROADMAP;
+			break;
+		}
+	}
+
+	//Die GoogleKarte erzeugen, in content_map DIV mit den Optionen
+	var googleMap = new google.maps.Map($('#content_map')[0], mapOverviewOptions),
+		myPositionIcon = new google.maps.MarkerImage(	"res/drawable/dot.png",			//Pfad zum Bild 
+														new google.maps.Size(20,20), 	//Image Groesse
+														new google.maps.Point(0,0), 	//Startpunkt
+														new google.maps.Point(10,10)), 	//Anker, Welcher Pixel auf die GPS-Position gesetzt wird 
+	
+		//Der Eigene PositionsMarker
+		myPositionMarker = new google.maps.Marker({
+			position : latlng,
+			map : googleMap,
+			icon : myPositionIcon
+		}),
+
+		//Der Kreis um den PositionsMarker
+		myCircle = new google.maps.Circle({
+			center: latlng,
+			radius: 20,
+			map: googleMap,
+			  strokeColor: "#0000FF",
+			  strokeOpacity: 0.8,
+			  strokeWeight: 2,
+			  fillColor: "#0000FF",
+			  fillOpacity: 0.35,
+		});
+
+	// Singleton Pattern
+	instance = this;
+	MapOverview = function(){
+		return instance;
+	}
+	MapOverview.prototype = this;
+	instance = new MapOverview();
+	instance.constructor = MapOverview;
+
+	//Die Karte Aktivieren
+	this.activate = function() {
+		setStatus("running");
+    	localStorage[localStorage["game"]+"currentMission"] = ID;
+		$.mobile.changePage($('#page_map'), "slide");
+		google.maps.event.trigger(googleMap, 'resize');
+		$('#content_map').height($('#page_map').innerHeight() - $('#header_map').outerHeight()-30);
+		watchID = navigator.geolocation.watchPosition(updatePosition, gpsError, gpsOptions);
+		$.mobile.loadingMessage = "Warte auf GPS Signal."
+		$.mobile.showPageLoadingMsg();
+	}
+	
+	
+	//ALT muss rausgenommen, MAP ist bald keine Mission mehr sondern ein TOOL 
+	this.play = function() {
+		this.activate();
+	}
+	
+	this.decativate = function(){
+		//deaktiviert das GPS wenn es nicht benoetigt wird
+		navigator.geolocation.clearWatch(watchID);
+		//Macht das Sinn?
+		setStatus("success");
+	}
+
+	
+	function initialCenterMap(myPosition){
+		latlng = google.maps.LatLng(myPosition.coords.latitude, myPosition.coords.longitude);
+		myPositionMarker.setPosition(latlng);
+		googleMap.setCenter(latlng);
+		myCircle.setCenter(latlng);
+		myCircle.setRadius(myPosition.coords.accuracy);
+	}
+	
+	function updatePosition(myPosition) {
+		if (myPosition.coords.accuracy < myAccuracy){ //Feste Genauigkeit
+			$.mobile.hidePageLoadingMsg();
+			latlng = new google.maps.LatLng(myPosition.coords.latitude, myPosition.coords.longitude);
+			myPositionMarker.setPosition(latlng);
+			myCircle.setCenter(latlng);
+			myCircle.setRadius(myPosition.coords.accuracy);
+			googleMap.setCenter(latlng);
+			globalGameHandler.updateHotspotsDistance(myPosition.coords.latitude, myPosition.coords.longitude);
+		}
+		else {
+			$.mobile.hidePageLoadingMsg();	
+			$.mobile.loadingMessage = "Warte auf GPS Signal. Accuracy: " + myPosition.coords.accuracy;
+			$.mobile.showPageLoadingMsg();	
+		}
+	}
+	
+	function gpsError(){
+		alert("Fehler beim GPS!");
+	}
+	
+	function setStatus(status){
+		localStorage[localStorage["game"]+ID] = status;
+	}
+	
+	this.getStatus = function(){
+		return localStorage[localStorage["game"]+ID];
+	}
+	
+	this.addMarker = function(id, visible, lat, long, image) {
+		var latlng = new google.maps.LatLng(lat, long);
+		var icon = BASEURL + "/res/drawable/icon.png";
+		if (image) {
+			icon = GAMEURL + image;
+			
+			//Caching des Bildes
+			if (document.images) {
+				var imageCache = new Image();
+				imageCache.src = GAMEURL + image;
+			}
+		}
+		googleMarkers[id] = new google.maps.Marker({
+			position : latlng,
+			title : id,
+			icon : icon
+		});
+		
+		if (GEOQUEST_RESUME){
+			if (localStorage[localStorage["game"]+"_Marker_"+id] === "true"){	
+				googleMarkers[id].setMap(googleMap);
+			}
+			else{
+				googleMarkers[id].setMap(null);
+			}
+		}
+		else {
+			if (visible){
+				googleMarkers[id].setMap(googleMap);
+				localStorage[localStorage["game"]+"_Marker_"+id] = "true";
+			}
+		}
+	};
+
+	this.showMarker = function(id) {
+		googleMarkers[id].setMap(googleMap);
+		localStorage[localStorage["game"]+"_Marker_"+id] = "true";
+	};
+
+	this.hideMarker = function(id) {
+		googleMarkers[id].setMap(null);
+		localStorage[localStorage["game"]+"_Marker_"+id] = null;
+	};
+	return instance;
+}
