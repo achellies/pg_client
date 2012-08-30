298821e2c10b9775782aa31622cb1fbddaca7105
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 20:32:29 +0200

    redirect selenium output and display it immediately

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -3,4 +3,5 @@ pwd
 sudo sh -e /etc/init.d/xvfb start
 export DISPLAY=:99
 echo "Starting selenium server..."
-java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 & echo $! > /tmp/selenium.pid
+java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log & echo $! > /tmp/selenium.pid
+cat /tmp/selenium-output.log
