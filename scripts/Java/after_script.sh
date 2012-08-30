723197c8f159c0a542d609e5ec39fd99dab485c4
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 20:53:14 +0200

    removed output of error log as it is not created anymore

--- scripts/Java/after_script.sh
+++ scripts/Java/after_script.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 echo "Selenium log:" 
 cat /tmp/selenium-output.log
-echo "Selenium error log:" 
-cat /tmp/selenium-error.log
 kill -9 `cat /tmp/selenium.pid`
 sudo sh -e /etc/init.d/xvfb stop
