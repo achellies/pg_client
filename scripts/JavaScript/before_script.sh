49db0b02ab81c4382dc27a8ad571db3490ae57b0
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 18:16:09 +0200

    Debug statements

--- scripts/JavaScript/before_script.sh
+++ scripts/JavaScript/before_script.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+echo "we are:"
+whoami
+echo "we are here:"
 pwd
+echo " this is the content:"
+ls -la
+echo "now we try cd"
 cd /home/travis/builds/geoquest/pg_client/assets
+echo "now we are here:"
 pwd
