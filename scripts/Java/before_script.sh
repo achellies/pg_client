aedd23db0e150d80ff1bb9be670045d42c8a31bf
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 20:25:41 +0200

    removed selenium output redirect

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -3,4 +3,4 @@ pwd
 sudo sh -e /etc/init.d/xvfb start
 export DISPLAY=:99
 echo "Starting selenium server..."
-java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2> /tmp/selenium-error.log & echo $! > /tmp/selenium.pid
+java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 & echo $! > /tmp/selenium.pid
