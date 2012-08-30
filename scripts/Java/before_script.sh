08aff9b2cb4b7cabcf93b523c285ed3977a1f513
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 20:44:12 +0200

    try to wait for selenium

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -2,6 +2,6 @@
 pwd
 sudo sh -e /etc/init.d/xvfb start
 export DISPLAY=:99
-echo "Starting selenium server..."
-java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid
-cat /tmp/selenium-output.log
+echo "Starting Selenium server..."
+java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid & while ! nc -vz localhost 4444; do sleep 1; done
+echo "Selenium started..."
