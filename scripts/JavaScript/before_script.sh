f08ad8f38af39c533378ad191a3dfeb28563928b
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 18:24:22 +0200

    javascript is java??

--- scripts/JavaScript/before_script.sh
+++ scripts/JavaScript/before_script.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-echo "we are"
-whoami
-echo "we are here"
 pwd
-echo "this is the content"
-ls -l
-echo "now we try cd"
-cd /home/travis/builds/geoquest/pg_client/assets
-echo "now we are here:"
-pwd
-echo "let's cd again"
-cd /home/travis/builds/geoquest/pg_client/assets
-echo "and now we are here"
-pwd
+sudo sh -e /etc/init.d/xvfb start
+export DISPLAY=:99
+java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2> /tmp/selenium-error.log & echo $! > /tmp/selenium.pid
