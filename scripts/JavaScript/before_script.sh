214317065666f7ac95bd9eadf602d585c952fcd0
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 18:26:25 +0200

    Added functionality

--- scripts/JavaScript/before_script.sh
+++ scripts/JavaScript/before_script.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 pwd
-sudo sh -e /etc/init.d/xvfb start
-export DISPLAY=:99
-java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2> /tmp/selenium-error.log & echo $! > /tmp/selenium.pid
+cd assets
