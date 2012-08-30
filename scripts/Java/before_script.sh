0ab3291042e9311ae5e52dc64cb5b0d659706f5d
Matthias Molitor <matthias@matthimatiker.de>
2012-08-30 20:22:56 +0200

    added message

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -2,4 +2,5 @@
 pwd
 sudo sh -e /etc/init.d/xvfb start
 export DISPLAY=:99
+echo "Starting selenium server..."
 java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2> /tmp/selenium-error.log & echo $! > /tmp/selenium.pid
