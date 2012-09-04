8a6a8ded926ce73213e3b4adc0d5cbae1ce8e3e5
RB <rebedeub@yahoo.com>
2012-09-04 16:21:14 +0200

    travis refactoring and changed android target platform to 16 (4.1)

--- scripts/Java/before_install.sh
+++ scripts/Java/before_install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 sudo apt-get install xvfb
 wget -O /tmp/selenium-server-standalone-2.25.0.jar http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
+
+# download the latest android sdk and unzip
+pwd
+wget -O /tmp/android-sdk-linux.tgz http://dl.google.com/android/android-sdk_r20-linux.tgz
+cd /tmp/
+tar -zxf android-sdk-linux.tgz
+
+# setup your ANDROID_HOME and PATH environment variables
+export ANDROID_SDK_HOME=/tmp/android-sdk-linux
+export PATH=${PATH}:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools
+
+# only update the sdk for the tools and platform-tools (1,2) and whatever api level
+# you are building for android (run "android list sdk" to get the full list. 9 = 2.3.3 or API level 10
+android update sdk --filter 1 --no-ui --force
