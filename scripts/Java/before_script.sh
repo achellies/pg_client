23a094221da60198a08ab21b385ced77c7272f89
RB <rebedeub@yahoo.com>
2012-09-05 13:44:07 +0200

    Starting up selenium server before test script is executed

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sudo apt-get install xvfb
-wget -O /tmp/selenium-server-standalone-2.25.0.jar http://selenium.googlecode.com/files/selenium-server-standalone-2.25.0.jar
+pwd
+sudo sh -e /etc/init.d/xvfb start
+export DISPLAY=:99
+echo "Starting Selenium server..."
+java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid & while ! nc -vz localhost 4444; do sleep 1; done
+echo "Selenium started..."
