63bbbc707231b17b20471061d54ce669546425f8
RB <rebedeub@yahoo.com>
2012-09-05 11:56:35 +0200

    travis maven debugging

--- scripts/Java/install.sh
+++ scripts/Java/install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-mvn install -B --quiet -DskipTests=true
+
+echo 'Android SDK Path:'
+echo $ANDROID_SDK_HOME
+
+mvn install -B --quiet -DskipTests=true -Dandroid.sdk.path=$ANDROID_SDK_HOME
 #echo 'install skipped'
