158cb99c03ce3a6c3a47c33d0b95d78ee1123d87
RB <rebedeub@yahoo.com>
2012-09-05 12:08:24 +0200

    added platform-tools to android sdk install

--- scripts/Java/before_install.sh
+++ scripts/Java/before_install.sh
@@ -14,4 +14,4 @@ export PATH=${PATH}:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools
 
 # only update the sdk for the tools and platform-tools (1,2) and whatever api level
 # you are building for android (run "android list sdk" to get the full list. 9 = 2.3.3 or API level 10
-android update sdk --filter android-10 -u --force
+android update sdk --filter android-10,platform-tools -u --force
