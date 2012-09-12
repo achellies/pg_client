25a2443dd72d38f72dda166af50a53cf54b0653d
Pascal Bihler <bihler@cs.uni-bonn.de>
2012-09-12 13:58:31 +0200

    Added new coverage2text from node_server

--- scripts/JavaScript/after_script.sh
+++ scripts/JavaScript/after_script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd GeoQuestPhoneGapClient/assets
 npm run-script coverage
-w3m -dump build/coverage.html
+perl ../../scripts/coverage2text.pl build/coverage.html
--- scripts/JavaScript/before_install.sh
+++ scripts/JavaScript/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pwd
 sudo apt-get install jscoverage
-sudo apt-get install w3m
+sudo apt-get install libhtml-parser-perl
 cd GeoQuestPhoneGapClient/assets
 npm install
