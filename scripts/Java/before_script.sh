68fbf2d11d831aaa2caae0b688abaaab44b3f12a
Pascal Bihler <bihler@cs.uni-bonn.de>
2012-09-12 17:41:51 +0200

    nohuped services

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 pwd
-Xvfb :99 -nolisten tcp & echo $! > /tmp/xvfb.pid
+nohup Xvfb :99 -nolisten tcp & echo $! > /tmp/xvfb.pid
 export DISPLAY=:99
+sleep 2
 echo "Starting Selenium server..."
-java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid & while ! nc -vz localhost 4444; do sleep 1; done
+nohup java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid & while ! nc -vz localhost 4444; do sleep 1; done
 echo "Selenium started..."
