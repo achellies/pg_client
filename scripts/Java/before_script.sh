fd28f8df4d842b5222bd804947936fa086694d4f
Pascal Bihler <bihler@cs.uni-bonn.de>
2012-09-13 16:56:08 +0200

    Fixedsmall issue with selenium output

--- scripts/Java/before_script.sh
+++ scripts/Java/before_script.sh
@@ -4,5 +4,6 @@ nohup Xvfb :99 -nolisten tcp & echo $! > /tmp/xvfb.pid
 export DISPLAY=:99
 sleep 2
 echo "Starting Selenium server..."
+[ -e /tmp/selenium-output.log ] && rm /tmp/selenium-output.log
 nohup java -jar /tmp/selenium-server-standalone-2.25.0.jar -port 4444 > /tmp/selenium-output.log 2>&1 & echo $! > /tmp/selenium.pid & while ! nc -vz localhost 4444; do sleep 1; done
 echo "Selenium started..."
