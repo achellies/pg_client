ea5572e95839529f036d3b13ca35816e2e80f5a9
shubham bansal <shubhambans@gmail.com>
2012-09-20 16:50:14 +0200

    Add QRTag support for display some message. Remove the image content text.

--- GeoQuestPhoneGapClient/assets/www/res/js/missions/QRTagReading.js
+++ GeoQuestPhoneGapClient/assets/www/res/js/missions/QRTagReading.js
@@ -9,53 +9,66 @@ function QRTagReadingMission(missionNode){
 	console.log('missionNode.taskdescription = ' + missionNode.taskdescription);
 	console.log('missionNode.expectedContent = ' + missionNode.expectedContent);
 	
+	var buttontext = "Scan ...";
+	var ID = missionNode.id;
+	var taskdescription = missionNode.taskdescription;
+	var endButtonText = "Next ..."; //default
+	var outroSuccessText = "Correct!";
+	var outroFailText = "Wrong";
+	if(missionNode.outroSuccessText)
+		outroSuccessText = missionNode.outroSuccessText;
+	if(missionNode.outroFailText)
+		outroFailText = missionNode.outroFailText;
+	if(missionNode.endButtonText)
+		endButtonText = missionNode.endButtonText;
 	
-	var taskdescription,
-		initial_image,
-		endbuttontext = "Weiter ...", //default
-		buttontext = "Scan ...";
-	
-	
-		taskdescription = missionNode.taskdescription +'\n' + missionNode.expectedContent;
+	function setStatus(status){
+		localStorage[localStorage["game"]+ID] = status;
+	}
+
+	this.play = function(){
+		console.log('taskdescription = ' + taskdescription);
+		console.log('missionNode.taskdescription = ' + missionNode.taskdescription);
+		
+		$.mobile.changePage($('#page_QRTagReading'), "slide");
+		$('#text_QRTagReading').empty().append(taskdescription);
+		$('#footer_QRTagReading').empty().append("<h3>" + buttontext + "</h3>" );
 		
 		
-		this.play = function(){
-			console.log('taskdescription = ' + taskdescription);
-			console.log('missionNode.taskdescription = ' + missionNode.taskdescription);
+		$('#footer_QRTagReading').bind('click', function(){
+			$('#footer_QRTagReading').unbind();
 			
-			$.mobile.changePage($('#page_QRTagReading'), "slide");
-			$('#text_QRTagReading').empty().append(taskdescription);
-			$('#footer_QRTagReading').empty().append("<h3>" + "Scan ..." + "</h3>" );
-			
-			
-			$('#footer_QRTagReading').bind('click', function(){
-				$('#footer_QRTagReading').unbind();
+			cordova.exec(function(result) {
+				console.log('missionNode.expectedContent = ' + missionNode.expectedContent);
+				console.log('result.text = ' + result.text);
+				$('#image_QRTagReading').empty();
+				$('#footer_QRTagReading').empty().append("<h3>" + endButtonText + "</h3>" );
 				
-				cordova.exec(function(result) {
-					console.log('missionNode.expectedContent = ' + missionNode.expectedContent);
-					console.log('result.text = ' + result.text);
+				if(result.text == missionNode.expectedContent){
+					$('#text_QRTagReading').empty().append(outroSuccessText);
+					$('#footer_QRTagReading').bind('click', function(){
+						$('#footer_QRTagReading').unbind();
+						setStatus(STATUS_SUCCESS);
+						globalGameHandler.finishMission(missionNode.id);
+					});
+				} else {
+					$('#text_QRTagReading').empty().append(outroFailText);
+					$('#footer_QRTagReading').bind('click', function(){
+						$('#footer_QRTagReading').unbind();
+						setStatus(STATUS_FAILED);
+						globalGameHandler.startMission(missionNode.id);
+					});
 					
-					if(result.text == missionNode.expectedContent){
-						$('#text_QRTagReading').empty().append(result.text);
-						$('#image_QRTagReading').empty();
-						$('#footer_QRTagReading').empty().append("<h3>" + endbuttontext + "</h3>" );
-						
-						$('#footer_QRTagReading').bind('click', function(){
-							$('#footer_QRTagReading').unbind();
-							globalGameHandler.finishMission(missionNode.id);
-						});
-					}else{
-						alert('Please scan again! Code does not match.');
-						$('#footer_QRTagReading').bind('click', function(){
-							$('#footer_QRTagReading').unbind();
-							globalGameHandler.startMission(missionNode.id);
-						});
-						
-					}
-				    }, function(error) {
-				    	alert("Fehler beim Lesen!");
-			    }, "BarcodeScanner", "scan", []);
-			});	
-		
-        };
+				}
+		    }, 
+		    function(error) {
+				setStatus(STATUS_FAILED);
+		    	alert("Error on reading!");
+		    },
+		    "BarcodeScanner",
+		    "scan",
+		    []);
+		});	
+	
+    };
 }
