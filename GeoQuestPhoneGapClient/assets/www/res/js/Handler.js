f179b1424e80e5f0e7f377b9ec5ddd10f77665ba
Johannes Zollmann <zollmann@uni-bonn.de>
2012-09-20 12:16:06 +0200

    removed flickering after choosing game page

--- GeoQuestPhoneGapClient/assets/www/res/js/Handler.js
+++ GeoQuestPhoneGapClient/assets/www/res/js/Handler.js
@@ -28,13 +28,13 @@ function Handler() {
 	 */
 	this.parseJsonToOldFormat = function(gameJson){
 		//reset gameHandlerObject
-		hotspots = {}; // Dieses Objekt speichert die HotSpots
-		missions = {}; // Dieses Objekt speichert die Missionen
-		onStart = {}; // Dieses Objekt speichert die onStart Knoten zur Mission
-		onEnd = {}; // Dieses Objekt speichert die onEnd Knoten zur Mission
-		onEnter = {}; // Dieses Objekt speichert die onEnter Knoten zum HotSpot
-		onLeave = {}; // Dieses Objekt speichert die onLeave Knoten zum HotSpot
-		firstMission = null;
+//		hotspots = {}; // Dieses Objekt speichert die HotSpots
+//		missions = {}; // Dieses Objekt speichert die Missionen
+//		onStart = {}; // Dieses Objekt speichert die onStart Knoten zur Mission
+//		onEnd = {}; // Dieses Objekt speichert die onEnd Knoten zur Mission
+//		onEnter = {}; // Dieses Objekt speichert die onEnter Knoten zum HotSpot
+//		onLeave = {}; // Dieses Objekt speichert die onLeave Knoten zum HotSpot
+//		firstMission = null;
 		
 		GAMEFILENAME = gameJson.name;
 		if (mapAvailable){
