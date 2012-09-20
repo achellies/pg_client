01fff76a2aeac5cbf390f0bcf820f01b2ec7d368
shubham bansal <shubhambans@gmail.com>
2012-09-20 10:53:46 +0200

    QR-Scan Mission completed in pg_client.

--- GeoQuestPhoneGapClient/assets/www/res/js/missions/QRTagReading.js
+++ GeoQuestPhoneGapClient/assets/www/res/js/missions/QRTagReading.js
@@ -9,11 +9,6 @@ function QRTagReadingMission(missionNode){
 	console.log('missionNode.taskdescription = ' + missionNode.taskdescription);
 	console.log('missionNode.expectedContent = ' + missionNode.expectedContent);
 	
-		
-	
-		/*var missionAttributes = missionNode.attributes,
-		ID = missionAttributes.getNamedItem("id").nodeValue,
-		mode = missionAttributes.getNamedItem("mode").nodeValue,*/
 	
 	var taskdescription,
 		initial_image,
@@ -23,22 +18,7 @@ function QRTagReadingMission(missionNode){
 	
 		taskdescription = missionNode.taskdescription +'\n' + missionNode.expectedContent;
 		
-		/*if (missionAttributes.getNamedItem("taskdescription")){
-			taskdescription=missionAttributes.getNamedItem("taskdescription").nodeValue;
-		}
 		
-		if (missionAttributes.getNamedItem("initial_image")){
-			initial_image=missionAttributes.getNamedItem("initial_image").nodeValue;
-		}
-		
-		if (missionAttributes.getNamedItem("endbuttontext")){
-			endbuttontext=missionAttributes.getNamedItem("endbuttontext").nodeValue;
-		}
-		
-		if (missionAttributes.getNamedItem("buttontext")){
-			buttontext=missionAttributes.getNamedItem("buttontext").nodeValue;
-		}
-        */
 		this.play = function(){
 			console.log('taskdescription = ' + taskdescription);
 			console.log('missionNode.taskdescription = ' + missionNode.taskdescription);
@@ -46,11 +26,6 @@ function QRTagReadingMission(missionNode){
 			$.mobile.changePage($('#page_QRTagReading'), "slide");
 			$('#text_QRTagReading').empty().append(taskdescription);
 			$('#footer_QRTagReading').empty().append("<h3>" + "Scan ..." + "</h3>" );
-			
-			/*if (initial_image){
-				$('#image_QRTagReading').empty().append("<img id='theImg' src='" + 
-														GAMEURL + initial_image + "'/>");
-				}*/
 			
 			
 			$('#footer_QRTagReading').bind('click', function(){
@@ -67,28 +42,13 @@ function QRTagReadingMission(missionNode){
 						
 						$('#footer_QRTagReading').bind('click', function(){
 							$('#footer_QRTagReading').unbind();
-							
-							var method = missionNode.onSuccess[0].actions[0].method;
-							if(method == 'endGame'){
-								globalGameHandler.startMission(null);
-							}else{
-								var newId = missionNode.onSuccess[0].actions[0].arguments.id;
-								console.log('new mission id = ' + newId);
-								globalGameHandler.startMission(newId);
-							}
+							globalGameHandler.finishMission(missionNode.id);
 						});
 					}else{
 						alert('Please scan again! Code does not match.');
 						$('#footer_QRTagReading').bind('click', function(){
 							$('#footer_QRTagReading').unbind();
-							var method = missionNode.onSuccess[0].actions[0].method;
-							if(method == 'endGame'){
-								globalGameHandler.startMission(null);
-							}else{
-								var newId = missionNode.onSuccess[0].actions[0].arguments.id;
-								console.log('new mission id = ' + newId);
-								globalGameHandler.startMission(newId);
-							}
+							globalGameHandler.startMission(missionNode.id);
 						});
 						
 					}
